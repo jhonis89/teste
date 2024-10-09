87064cab7da75d3657d6193f76fa9cce2cb815a9
jhonis89 <161889220+jhonis89@users.noreply.github.com>
2024-10-09 09:35:48 -0300

    Atualizar o Pagina-inicial.js

--- Pagina-inicial.js
+++ Pagina-inicial.js
@@ -1,51 +1,102 @@
-function Menu(){
+const tamanhoTela = window.matchMedia("(max-width: 800px)");
 
-    var barra = document.getElementById("barra-de-tarefas");
-    var menu = document.getElementById("menu-cel");
-    var simbolo = document.getElementById("botao-cel")
+function barraCel(){
+    var lista = document.getElementById("listaCel");
+    var btn = document.getElementById("btn");
 
-    if(barra.style.display === "inline-block"){
-        barra.style.display = "none";
-        menu.style.display = "none";
-        simbolo.innerHTML = "&#9776"
-
-    }
+    if (lista.style.display === "flex"){
+        lista.style.display = "none";
+        btn.innerHTML = "&#9776;"
+    }    
     else{
-        barra.style.display = "inline-block"
-        menu.style.display = "inline-block"
-        simbolo.innerHTML = "X"
-
+        lista.style.display = "flex";
+        btn.innerHTML = "X";
+        
     }
 }
 
-function Pesquisa(){
-    var texto = document.getElementById("texto-da-pesquisa-cel");
-    var caixa = document.getElementById("caixa-da-pesquisa-cel");
-    var botao_pesquisar = document.getElementById("botao-da-pesquisa-cel");
-    var botao_voltar = document.getElementById("botao-voltar-pesquisa-cel");
-    var topo = document.getElementById("top");
+function Abrir_pesquisa() {
+    var logo = document.getElementById("logo");
+    var input = document.getElementById("input_pesquisa");
+    var btn_voltar = document.getElementById("btn_voltar");
+    var btn_tarefas = document.getElementById("btn");
+    var pesq = document.getElementById("pesquisa");
 
-    /* SE A PESQUISA NÃO ESTÁ VISÍVEL */
-    if (texto.style.display === "none" || texto.style.display === "") {
-        topo.style.display = "none"; 
-        caixa.style.height = "6vh"; 
-        caixa.style.display = "flex"; 
-        botao_pesquisar.style.display = "inline-block"; 
-        texto.style.display = "block"; 
-        botao_voltar.style.display = "inline-block"; 
+    var logoDisplay = window.getComputedStyle(logo).display;
+
+    if (logoDisplay === "inline-block" || logoDisplay === "block"){
+        history.pushState({ aberto: true }, null, "");
+        pesq.style.width = "100vw";
+        logo.style.display = "none";
+        input.style.display = "inline-block";
+        btn_voltar.style.display = "inline-block";
+        btn_tarefas.style.display = "none";
     }
 }
 
-    /* SE A PESQUISA NÃO ESTÁ VISÍVEL */
-function VoltarPesquisa(){
-    var texto = document.getElementById("texto-da-pesquisa-cel");
-    var caixa = document.getElementById("caixa-da-pesquisa-cel");
-    var botao_voltar = document.getElementById("botao-voltar-pesquisa-cel");
-    var topo = document.getElementById("top");
+window.onpopstate = function(event) {
+    var logo = document.getElementById("logo");
+    var input = document.getElementById("input_pesquisa");
+    var btn_voltar = document.getElementById("btn_voltar");
+    var btn_tarefas = document.getElementById("btn");
 
-    topo.style.display = "flex"; 
-    caixa.style.display = "none"; 
-    texto.style.display = "none"; 
-    botao_voltar.style.display = "none";
+    if (event.state && event.state.aberto){
+        logo.style.display = "inline-block";
+        input.style.display = "none";
+        btn_voltar.style.display = "none";
+        btn_tarefas.style.display = "inline-block";
+    }
+}
+
+function Voltar_pesquisa() {
+    var logo = document.getElementById("logo");
+    var input = document.getElementById("input_pesquisa");
+    var btn_voltar = document.getElementById("btn_voltar");
+    var btn_tarefas = document.getElementById("btn");
+    var pesq = document.getElementById("pesquisa");
+    var logoDisplay = window.getComputedStyle(logo).display;
+
+    if (logoDisplay === "none"){
+        pesq.style.width = "auto";
+        logo.style.display = "inline-block";
+        input.style.display = "none";
+        btn_voltar.style.display = "none";
+        btn_tarefas.style.display = "inline-block";
+    }
+}
+
+function ajustarLayout() {
+    var logo = document.getElementById("logo");
+    var input = document.getElementById("input_pesquisa");
+    var btn_voltar = document.getElementById("btn_voltar");
+    var btn_tarefas = document.getElementById("btn");
+    var imgOn = document.getElementById("imgOn")
+    var pesq = document.getAnimations("pesquisa")
+
+    if (tamanhoTela.matches) {
+
+        imgOn.onclick = Abrir_pesquisa;
+
+        if (window.getComputedStyle(input).display === "inline-block") {
+            btn_voltar.style.display = "inline-block";
+            btn_tarefas.style.display = "none";
+        }
+        
+        else{
+            btn_tarefas.style.display = "inline-block";
+            input.style.display = "none";
+ 
+        }
+    }
     
+    else{
+        logo.style.display = "inline-block";
+        input.style.display = "inline-block"; 
+        btn_voltar.style.display = "none";
+        btn_tarefas.style.display = "none"; 
+        imgOn.onclick = null;
+    }
 }
+
+ajustarLayout();
+tamanhoTela.addListener(ajustarLayout);
